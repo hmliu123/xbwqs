fb342701202b8e6820a646c13ba93d5380b124ca
hmliu123 <110088093+hmliu123@users.noreply.github.com>
2025-11-30 11:26:45 +0800

    Update hbwqs_stan_models.R

--- R/hbwqs_stan_models.R
+++ R/hbwqs_stan_models.R
@@ -4,25 +4,25 @@
 #'
 hbwqs_covmod_contY <- "
 data {
-  int<lower=0> N;     \\ total number of observations
-  int<lower=0> J;     \\ number of cohorts
-  int<lower=0> K;     \\ number of elements in exposure mixture
-  int<lower=0> M;     \\ number of other covariates
-  matrix[N,K] X_matr; \\ exposure matrix
-  matrix[N,M] Z_matr; \\ covariates matrix
-  int cohort[N];      \\ cohort indicator vector
-  vector[N] y;        \\ continuous outcome
+  int<lower=0> N;     // total number of observations
+  int<lower=0> J;     // number of cohorts
+  int<lower=0> K;     // number of elements in exposure mixture
+  int<lower=0> M;     // number of other covariates
+  matrix[N,K] X_matr; // exposure matrix
+  matrix[N,M] Z_matr; // covariates matrix
+  int cohort[N];      // cohort indicator vector
+  vector[N] y;        // continuous outcome
 }
 parameters {
-  vector[J] a;            \\ cohort-specific intercepts
-  vector[J] b;            \\ cohort-specific slopes
-  real mu_a;              \\ overall intercept mean
-  real<lower=0> sigma_a;  \\ overall intercept sd
-  real mu_b;              \\ overall slope mean
-  real<lower=0> sigma_b;  \\ overall slope sd
-  vector[M] delta;        \\ covariates coefs
-  simplex[K] w;           \\ exposure mixture weights
-  real<lower=0> sigma;    \\ sd param for outcome model
+  vector[J] a;            // cohort-specific intercepts
+  vector[J] b;            // cohort-specific slopes
+  real mu_a;              // overall intercept mean
+  real<lower=0> sigma_a;  // overall intercept sd
+  real mu_b;              // overall slope mean
+  real<lower=0> sigma_b;  // overall slope sd
+  vector[M] delta;        // covariates coefs
+  simplex[K] w;           // exposure mixture weights
+  real<lower=0> sigma;    // sd param for outcome model
   vector<lower = 0.0001>[K] Dir_alpha; // alphas parameters of the weights
 }
 transformed parameters {
@@ -50,22 +50,22 @@ generated quantities {
 "
 hbwqs_nocovmod_contY <- "
 data {
-  int<lower=0> N;     \\ total number of observations
-  int<lower=0> J;     \\ number of cohorts
-  int<lower=0> K;     \\ number of elements in exposure mixture
-  matrix[N,K] X_matr; \\ exposure matrix
-  int cohort[N];      \\ cohort indicator vector
-  vector[N] y;        \\ continuous outcome
+  int<lower=0> N;     // total number of observations
+  int<lower=0> J;     // number of cohorts
+  int<lower=0> K;     // number of elements in exposure mixture
+  matrix[N,K] X_matr; // exposure matrix
+  int cohort[N];      // cohort indicator vector
+  vector[N] y;        // continuous outcome
 }
 parameters {
-  vector[J] a;            \\ cohort-specific intercepts
-  vector[J] b;            \\ cohort-specific slopes
-  real mu_a;              \\ overall intercept mean
-  real<lower=0> sigma_a;  \\ overall intercept sd
-  real mu_b;              \\ overall slope mean
-  real<lower=0> sigma_b;  \\ overall slope sd
-  simplex[K] w;           \\ exposure mixture weights
-  real<lower=0> sigma;    \\ sd param for outcome model
+  vector[J] a;            // cohort-specific intercepts
+  vector[J] b;            // cohort-specific slopes
+  real mu_a;              // overall intercept mean
+  real<lower=0> sigma_a;  // overall intercept sd
+  real mu_b;              // overall slope mean
+  real<lower=0> sigma_b;  // overall slope sd
+  simplex[K] w;           // exposure mixture weights
+  real<lower=0> sigma;    // sd param for outcome model
   vector<lower = 0.0001>[K] Dir_alpha; // alphas parameters of the weights
 }
 transformed parameters {
@@ -97,24 +97,24 @@ generated quantities {
 #'
 hbwqs_covmod_binY <- "
 data {
-  int<lower=0> N;     \\ total number of observations
-  int<lower=0> J;     \\ number of cohorts
-  int<lower=0> K;     \\ number of elements in exposure mixture
-  int<lower=0> M;     \\ number of other covariates
-  matrix[N,K] X_matr; \\ exposure matrix
-  matrix[N,M] Z_matr; \\ covariates matrix
-  int cohort[N];      \\ cohort indicator vector
-  int<lower=0, upper=1> y[N];        \\ binary outcome
+  int<lower=0> N;     // total number of observations
+  int<lower=0> J;     // number of cohorts
+  int<lower=0> K;     // number of elements in exposure mixture
+  int<lower=0> M;     // number of other covariates
+  matrix[N,K] X_matr; // exposure matrix
+  matrix[N,M] Z_matr; // covariates matrix
+  int cohort[N];      // cohort indicator vector
+  int<lower=0, upper=1> y[N];        // binary outcome
 }
 parameters {
-  vector[J] a;            \\ cohort-specific intercepts
-  vector[J] b;            \\ cohort-specific slopes
-  real mu_a;              \\ overall intercept mean
-  real<lower=0> sigma_a;  \\ overall intercept sd
-  real mu_b;              \\ overall slope mean
-  real<lower=0> sigma_b;  \\ overall slope sd
-  vector[M] delta;        \\ covariates coefs
-  simplex[K] w;           \\ exposure mixture weights
+  vector[J] a;            // cohort-specific intercepts
+  vector[J] b;            // cohort-specific slopes
+  real mu_a;              // overall intercept mean
+  real<lower=0> sigma_a;  // overall intercept sd
+  real mu_b;              // overall slope mean
+  real<lower=0> sigma_b;  // overall slope sd
+  vector[M] delta;        // covariates coefs
+  simplex[K] w;           // exposure mixture weights
   vector<lower = 0.0001>[K] Dir_alpha; // alphas parameters of the weights
 }
 transformed parameters {
@@ -142,21 +142,21 @@ generated quantities {
 
 hbwqs_nocovmod_binY <- "
 data {
-  int<lower=0> N;     \\ total number of observations
-  int<lower=0> J;     \\ number of cohorts
-  int<lower=0> K;     \\ number of elements in exposure mixture
-  matrix[N,K] X_matr; \\ exposure matrix
-  int cohort[N];      \\ cohort indicator vector
-  int<lower=0, upper=1> y[N];        \\ binary outcome
+  int<lower=0> N;     // total number of observations
+  int<lower=0> J;     // number of cohorts
+  int<lower=0> K;     // number of elements in exposure mixture
+  matrix[N,K] X_matr; // exposure matrix
+  int cohort[N];      // cohort indicator vector
+  int<lower=0, upper=1> y[N];        // binary outcome
 }
 parameters {
-  vector[J] a;            \\ cohort-specific intercepts
-  vector[J] b;            \\ cohort-specific slopes
-  real mu_a;              \\ overall intercept mean
-  real<lower=0> sigma_a;  \\ overall intercept sd
-  real mu_b;              \\ overall slope mean
-  real<lower=0> sigma_b;  \\ overall slope sd
-  simplex[K] w;           \\ exposure mixture weights
+  vector[J] a;            // cohort-specific intercepts
+  vector[J] b;            // cohort-specific slopes
+  real mu_a;              // overall intercept mean
+  real<lower=0> sigma_a;  // overall intercept sd
+  real mu_b;              // overall slope mean
+  real<lower=0> sigma_b;  // overall slope sd
+  simplex[K] w;           // exposure mixture weights
   vector<lower = 0.0001>[K] Dir_alpha; // alphas parameters of the weights
 }
 transformed parameters {
@@ -187,24 +187,24 @@ generated quantities {
 
 hbwqs_covmod_countY <- "
 data {
-  int<lower=0> N;     \\ total number of observations
-  int<lower=0> J;     \\ number of cohorts
-  int<lower=0> K;     \\ number of elements in exposure mixture
-  int<lower=0> M;     \\ number of other covariates
-  matrix[N,K] X_matr; \\ exposure matrix
-  matrix[N,M] Z_matr; \\ covariates matrix
-  int cohort[N];      \\ cohort indicator vector
-  int<lower=0> y[N];        \\ count outcome
+  int<lower=0> N;     // total number of observations
+  int<lower=0> J;     // number of cohorts
+  int<lower=0> K;     // number of elements in exposure mixture
+  int<lower=0> M;     // number of other covariates
+  matrix[N,K] X_matr; // exposure matrix
+  matrix[N,M] Z_matr; // covariates matrix
+  int cohort[N];      // cohort indicator vector
+  int<lower=0> y[N];        // count outcome
 }
 parameters {
-  vector[J] a;            \\ cohort-specific intercepts
-  vector[J] b;            \\ cohort-specific slopes
-  real mu_a;              \\ overall intercept mean
-  real<lower=0> sigma_a;  \\ overall intercept sd
-  real mu_b;              \\ overall slope mean
-  real<lower=0> sigma_b;  \\ overall slope sd
-  vector[M] delta;        \\ covariates coefs
-  simplex[K] w;           \\ exposure mixture weights
+  vector[J] a;            // cohort-specific intercepts
+  vector[J] b;            // cohort-specific slopes
+  real mu_a;              // overall intercept mean
+  real<lower=0> sigma_a;  // overall intercept sd
+  real mu_b;              // overall slope mean
+  real<lower=0> sigma_b;  // overall slope sd
+  vector[M] delta;        // covariates coefs
+  simplex[K] w;           // exposure mixture weights
   vector<lower = 0.0001>[K] Dir_alpha; // alphas parameters of the weights
 }
 transformed parameters {
@@ -231,21 +231,21 @@ generated quantities {
 "
 hbwqs_nocovmod_countY <- "
 data {
-  int<lower=0> N;     \\ total number of observations
-  int<lower=0> J;     \\ number of cohorts
-  int<lower=0> K;     \\ number of elements in exposure mixture
-  matrix[N,K] X_matr; \\ exposure matrix
-  int cohort[N];      \\ cohort indicator vector
-  int<lower=0> y[N];        \\ count outcome
+  int<lower=0> N;     // total number of observations
+  int<lower=0> J;     // number of cohorts
+  int<lower=0> K;     // number of elements in exposure mixture
+  matrix[N,K] X_matr; // exposure matrix
+  int cohort[N];      // cohort indicator vector
+  int<lower=0> y[N];        // count outcome
 }
 parameters {
-  vector[J] a;            \\ cohort-specific intercepts
-  vector[J] b;            \\ cohort-specific slopes
-  real mu_a;              \\ overall intercept mean
-  real<lower=0> sigma_a;  \\ overall intercept sd
-  real mu_b;              \\ overall slope mean
-  real<lower=0> sigma_b;  \\ overall slope sd
-  simplex[K] w;           \\ exposure mixture weights
+  vector[J] a;            // cohort-specific intercepts
+  vector[J] b;            // cohort-specific slopes
+  real mu_a;              // overall intercept mean
+  real<lower=0> sigma_a;  // overall intercept sd
+  real mu_b;              // overall slope mean
+  real<lower=0> sigma_b;  // overall slope sd
+  simplex[K] w;           // exposure mixture weights
   vector<lower = 0.0001>[K] Dir_alpha; // alphas parameters of the weights
 }
 transformed parameters {
